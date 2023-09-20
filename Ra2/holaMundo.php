189bf00ec6f64e7be17ac5daef9266487f79cc3e
JavierPA3 <a21poarja@iesgrancapitan.org>
2023-09-20 10:16:36 +0200

    Añadiendo el tercer hola mundo

--- Ra2/holaMundo.php
+++ Ra2/holaMundo.php
@@ -1,3 +1,18 @@
-<?php
-    echo "hola mundo!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php // Apertura de comentario php
+    echo "¡Hola mundo!"; // Mostrar en la pagina web hola mundo
 ?>
+<br>
+<br>
+<a href='https://github.com/JavierPA3/DesarrolloEntornoServidor/blob/main/Ra2/holaMundo.php'>Enlace al github</a>
+</body>
+</html>
+
+
