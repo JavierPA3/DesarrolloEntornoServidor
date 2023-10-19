288d663ee97b12b534b988b5f967b44c6bb80c0f
JavierPA3 <91657168+JavierPA3@users.noreply.github.com>
2023-10-19 22:14:02 +0200

    Update Ejercicio3.php

--- Ra2/Tema3Ejercicios/EjercicioArray/Ejercicio3.php
+++ Ra2/Tema3Ejercicios/EjercicioArray/Ejercicio3.php
@@ -1,84 +1,26 @@
+<?php
+$nombreAlumnos = array('Javier Postigo Arévalo', 'Adrián González Martínez', 'Víctor Fernández España', 'Hector Cevallos Paredes', 'Ángel Fernández Ariza', 'Antonio Carmona Bascon', 'Adrián Cordovero', 'Alejandro', 'Andres Rodríguez', 'Ángel Cubero Olivares', 'Dani', 'Edu', 'Galisteo', 'Laura', 'Manu', 'Pablo Mérida Velasco', 'Alejandro Priego Izquierdo', 'Jesús Frías', '');
+
+if (isset($_POST['seleccionar'])) {
+    $alumnoSeleccionado = $nombreAlumnos[array_rand($nombreAlumnos)];
+    echo "<h1>Alumno seleccionado: " . $alumnoSeleccionado . "</h1>";
+}
+?>
+
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Selección Aleatoria de Alumno</title>
-</head>
-<body>
-    <h1>Alumno Aleatorio</h1>
-    <p>Nombre: <span id="nombreAlumno"></span></p>
-    <button id="seleccionarAlumno">Seleccionar Alumno Aleatorio</button>
-    <script>
-        var alumnos = [
-            {
-                nombre: "Javier Postigo Arévalo",
-            },
-            {
-                nombre: "Adrián González Martínez",
-            },
-            {
-                nombre: "Víctor Fernández España",
-            },
-            {
-                nombre: "Ángel Ariza",
-            },
-            {
-                nombre: "Antonio Carmona",
-            },
-            {
-                nombre: "Quique Ruz",
-            },
-            {
-                nombre: "Priego",
-            },
-            {
-                nombre: "Andrés",
-            },
-            {
-                nombre: "Pablo Mérida",
-            },
-            {
-                nombre: "Eduardo",
-            },
-            {
-                nombre: "Adrian Cordovero",
-            },
-            {
-                nombre: "Alex",
-            },
-            {
-                nombre: "Ángel Cubero Olivares",
-            },
-            {
-                nombre: "Dani",
-            },
-            {
-                nombre: "Galisteo",
-            },
-            {
-                nombre: "Lai",
-            },
-            {
-                nombre: "Manu",
-            },
-            {
-                nombre: "Frias",
-            },
-            {
-                nombre: "Willy",
-            },
-            {
-                nombre: "Héctor",
-            },
-        ];
 
-        function seleccionarAlumnoAleatorio() {
-            var indiceAleatorio = Math.floor(Math.random() * alumnos.length);
-            var alumnoSeleccionado = alumnos[indiceAleatorio];
-            document.getElementById("nombreAlumno").textContent = alumnoSeleccionado.nombre;
-        }
-        document.getElementById("seleccionarAlumno").addEventListener("click", seleccionarAlumnoAleatorio);
-        seleccionarAlumnoAleatorio();
-    </script>
+<head>
+    <title>Seleccionar Alumno Aleatorio</title>
+</head>
+
+<body>
+    <h1>Seleccionar Alumno Aleatorio</h1>
+    <form method="post">
+        <input type="submit" name="seleccionar" value="Generar un alumno aleatorio">
+    </form>
+    <div id="resultado">
+    </div>
 </body>
+
 </html>
